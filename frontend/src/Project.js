97a9601a1a2080ee1e820f84774e9fb4526eaa84
21tarun <tarunuday2000@gmail.com>
2023-04-12 12:26:50 +0530

    update in forntend

--- frontend/src/Project.js
+++ frontend/src/Project.js
@@ -352,7 +352,6 @@ function Project() {
                                 </div>
                             </div>
 
-                                
                             :<></>
     
                             
